fbab7238256b80dc6addfe691b67371ad5b92d0f
Vianney Depauw <vianney.depauw@gmail.com>
2013-01-25 23:59:55 +0100

    deleting fxml example and starting with new project

--- src/main/java/fr/utbm/petstock/MainApp.java
+++ src/main/java/fr/utbm/petstock/MainApp.java
@@ -1,36 +1,35 @@
 package fr.utbm.petstock;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MainApp extends Application {
 
-    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
-
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
-
-    public void start(Stage stage) throws Exception {
-
-        log.info("Starting Hello JavaFX and Maven demonstration application");
-
-        String fxmlFile = "/fxml/hello.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-
-        log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 400, 200);
-        scene.getStylesheets().add("/styles/styles.css");
-
-        stage.setTitle("Hello JavaFX and Maven");
+	@Override
+    public void start(Stage stage) throws Exception {        
+        Button bt = new Button("Print HelloWorld");
+        
+        bt.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("HelloWorld! :)");
+            }
+        });
+         
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 300, 150);
+        root.getChildren().add(bt);
+ 
+        stage.setTitle("HelloWorld in JavaFX 2.0");
         stage.setScene(scene);
         stage.show();
+    }
+ 
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
